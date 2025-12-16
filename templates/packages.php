dd77dcf26c8924670d6ff20379a2169538175f90
Krishna Wishvajith <krishnawishvajith@gmail.com>
2025-12-17 01:57:10 +0530

    updates

--- templates/packages.php
+++ templates/packages.php
@@ -6,6 +6,11 @@
  */
 
 if (!defined('ABSPATH')) exit;
+
+// FIXED: Direct URL construction without any functions
+$site_url = get_site_url(); // This gets your exact site URL
+$student_enroll_url = $site_url . '/?pz_enroll=student';
+$school_enroll_url = $site_url . '/?pz_enroll=school';
 ?>
 
 <div class="pz-packages-section">
@@ -64,12 +69,12 @@ if (!defined('ABSPATH')) exit;
             </div>
             
             <div class="pz-package-action">
-                <button class="pz-enroll-btn pz-student-btn" data-package="student">
+                <a href="<?php echo esc_url($student_enroll_url); ?>" class="pz-enroll-btn pz-student-btn">
                     Enrol Now
                     <svg class="arrow-icon" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
                     </svg>
-                </button>
+                </a>
             </div>
         </div>
         
@@ -133,12 +138,12 @@ if (!defined('ABSPATH')) exit;
             </div>
             
             <div class="pz-package-action">
-                <button class="pz-enroll-btn pz-school-btn" data-package="school">
+                <a href="<?php echo esc_url($school_enroll_url); ?>" class="pz-enroll-btn pz-school-btn">
                     Enrol Now
                     <svg class="arrow-icon" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"/>
                     </svg>
-                </button>
+                </a>
             </div>
         </div>
         
@@ -148,14 +153,3 @@ if (!defined('ABSPATH')) exit;
         <p>All packages include access to regularly updated materials and resources</p>
     </div>
 </div>
-
-<script>
-jQuery(document).ready(function($) {
-    $('.pz-enroll-btn').on('click', function(e) {
-        e.preventDefault();
-        var package = $(this).data('package');
-        var checkoutUrl = '<?php echo home_url('/pz-checkout/'); ?>?package=' + package;
-        window.location.href = checkoutUrl;
-    });
-});
-</script>
